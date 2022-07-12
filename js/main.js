a47b988b1648caf3f53a6eb1cf8d129eebbf6ae6
Morris Wang <morris1214@gmail.com>
2022-07-12 02:28:52 +0000

    Listen for 'submit' events and reset the form

--- js/main.js
+++ js/main.js
@@ -5,3 +5,21 @@ inputs.addEventListener('input', updatePhoto);
 function updatePhoto(e) {
   $image.src = inputs.value;
 }
+
+var $title = document.querySelector('#title');
+var $notes = document.querySelector('#notes');
+var $form = document.querySelector('form');
+$form.addEventListener('submit', submitForm);
+
+function submitForm(e) {
+  e.preventDefault();
+  var submission = {
+    title: $title.value,
+    photoURL: inputs.value,
+    notes: $notes.value,
+    nextEntryId: data.nextEntryId++
+  };
+  data.entries.push(submission);
+  $image.src = 'images/placeholder-image-square.jpg';
+  $form.reset();
+}
